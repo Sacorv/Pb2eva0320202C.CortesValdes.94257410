4ffab8b0fb59e6af1ea4cbed3be29fed323548bf
sacorv <scortesvaldes@alumno.unlam.edu.ar>
2020-11-24 19:55:39 -0300

    Se sube el proyecto

---
+++ src/ar/edu/unlam/pb220202c/eva03/Camion.java
@@ -0,0 +1,20 @@
+package ar.edu.unlam.pb220202c.eva03;
+
+public class Camion implements Imultable{
+	//Si es necesario Utilice herencia o implemente de Interfaces
+	//Se debe crear contructeres getters y Setters y los que crean conveniente
+	
+	
+	private Integer cantidadDeEjes;
+	//el Limite de velociadad para autos es de 80km
+	//en caso que supere dicho limite el este sera multado
+
+	
+	@Override
+	public Boolean enInfraccion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
---
+++ src/ar/edu/unlam/pb220202c/eva03/Imultable.java
@@ -0,0 +1,6 @@
+package ar.edu.unlam.pb220202c.eva03;
+
+public interface Imultable {
+
+	Boolean enInfraccion ();//si velocidad actual es superior a la velocidad max de la autopista devuleve true o false
+}
